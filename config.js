d2faba67a631f71c527fdffac2e9c5bc1b7bde38
Alexander Kudryavtsev <clear.meltdown@gmail.com>
2019-05-06 13:34:14 +0800

    config.local support

--- config.js
+++ config.js
@@ -1,10 +1,20 @@
-const config = {
-    port: 3000,
-    dbHost: 'localhost',
-    dbPort: 18889,
-    dbUser: 'ac_project_user',
-    dbName: 'ac_projects',
-    dbPassword: 'ac_gantt_project_password'
-};
+const fs = require('fs');
+
+const LOCAL_CONFIG = './config.local.js'
+
+const config = fs.existsSync(LOCAL_CONFIG) ?
+    require('./config.local') :
+    {
+        port: 3000,
+        dbHost: 'localhost',
+        dbPort: 18889,
+        dbUser: 'ac_project_user',
+        dbName: 'ac_projects',
+        dbPassword: 'ac_gantt_project_password'
+    }
 
 module.exports = config;
+
+
+
+
